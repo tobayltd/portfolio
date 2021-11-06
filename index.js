048cac22377c255011b3f5619b90a89f40d58f77
Tobay Ltd <92123370+tobayltd@users.noreply.github.com>
2021-11-06 12:23:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var person = prompt("Please enter your name", "Harry Potter");
 
-if (person != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! How are you today?";
-}
-
-var person1= prompt("Please enter your name", "Harry Potter");
-
-if (person1 != null) {
-  document.getElementById("demon").innerHTML =
-  "Hello " + person1 + "! How are you today?";
-}
