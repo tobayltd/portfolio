d61a93d915b9106f1f4617584041a2ad9b4e4071
Tobay Ltd <92123370+tobayltd@users.noreply.github.com>
2021-11-06 12:20:18 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var person = prompt("Please enter your name", "Harry Potter");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! How are you today?";
+}
+
+var person1= prompt("Please enter your name", "Harry Potter");
+
+if (person1 != null) {
+  document.getElementById("demon").innerHTML =
+  "Hello " + person1 + "! How are you today?";
+}
